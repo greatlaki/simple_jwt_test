fbf15e910cd028e50e2a8d8d8f01104ada83701a
greatlaki <greatlaki911@gmail.com>
2022-01-13 21:19:43 +0300

    Added log in to the browsable API

--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from .views import PostList, PostDetail
 
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
